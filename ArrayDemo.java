3d094f7041c68f20aa7fcc19f86fff1a86f2be14
Mrunal Badole <mrunalb1998@gmail.com>
2020-01-30 16:47:13 +0530

    First commit

---
+++ ArrayDemo.java
@@ -0,0 +1,28 @@
+package core.java1;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	int[] ar = {10,20,30,40,50};
+	
+		for (int i = 0; i < ar.length; i++) {
+			System.out.println(ar[i]);
+			
+		}
+		for (int i : ar) {
+			System.out.println(i);
+		}*/
+		int[][] jaggu = {{1,2},{3,4,5},{6,7,8,9}};
+		for (int i = 0; i < jaggu.length; i++) {
+			for (int j = 0; j < jaggu[i].length; j++) {
+				System.out.println(jaggu[i][j]+ "\t");
+			}
+				
+			}
+			
+		}
+
+	}
+
+
